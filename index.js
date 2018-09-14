291969e9ee082ae13cd8a609a7a2993eb67b99b4
Frank Zhou <frank.zhou@247-inc.com>
2018-09-14 06:02:20 -0700

    Is it true that git could not auto detect what changed?

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+var express = require('express');
+var app = express();
+ 
+//setting middleware
+app.use(express.static('www'));
+
+app.use('/html', express.static(__dirname + '/html'));
+app.use('/cgi-bin',express.static(__dirname + '/cgi-bin')); //Serves resources from www folder
+ 
+var server = app.listen(5050);
+
 var http = require('http'),
     fs = require('fs');
 
 
-fs.readFile('./html/index.html', function (err, html) {
+fs.readFile('./www/html/index.html', function (err, html) {
     if (err) {
         throw err; 
     }       
@@ -10,5 +21,5 @@ fs.readFile('./html/index.html', function (err, html) {
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
-    }).listen(8000);
+    }).listen(8080);
 });
